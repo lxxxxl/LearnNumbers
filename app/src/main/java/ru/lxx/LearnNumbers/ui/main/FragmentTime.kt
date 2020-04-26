a9e26e26bed0148248cbe0258522e23b81c486bd
Your Name <you@example.com>
2020-04-26 11:51:48 +0300

    Time tab logic

--- app/src/main/java/ru/lxx/LearnNumbers/ui/main/FragmentTime.kt
+++ app/src/main/java/ru/lxx/LearnNumbers/ui/main/FragmentTime.kt
@@ -3,12 +3,18 @@
 package ru.lxx.LearnNumbers.ui.main
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.fragment_numbers.*
 import ru.lxx.LearnNumbers.R
+import java.util.*
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +22,7 @@ import ru.lxx.LearnNumbers.R
 class FragmentTime : Fragment() {
 
     private lateinit var pageViewModel: PageViewModel
+    lateinit var mTTS: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +36,44 @@ class FragmentTime : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_time, container, false)
+
+        val textView_time: TextView = root.findViewById(R.id.textView_time)
+        textView_time.text = randomTime()
+
+
+        // Setup Text To Speech engine
+        mTTS = TextToSpeech(activity?.applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status != TextToSpeech.ERROR){
+                //if there is no error then set language
+                mTTS.language = Locale.getDefault()
+            }
+            else{
+                Toast.makeText(activity?.applicationContext, "Cannot initialise TTS engine", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Setup Text To Speech button
+        val imageView_tts: ImageView = root.findViewById(R.id.imageView_tts2)
+        imageView_tts.setOnClickListener{
+            val toSpeak = (textView_time.text as String).replace(':', ' ')
+            Toast.makeText(activity?.applicationContext, "Speaking $toSpeak", Toast.LENGTH_SHORT).show()
+            if (!mTTS.isSpeaking) {
+                mTTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+            }
+        }
+
+        // Setup random number generator
+        val imageView_random: ImageView = root.findViewById(R.id.imageView_random2)
+        imageView_random.setOnClickListener{
+            textView_time.text = randomTime()
+        }
         return root
+    }
+
+    private fun randomTime(): String{
+        val hours: Int = (0 until 23).random()
+        val mins: Int = (0 until 59).random()
+        return "%d:%02d".format(hours, mins)
     }
 
     companion object {
